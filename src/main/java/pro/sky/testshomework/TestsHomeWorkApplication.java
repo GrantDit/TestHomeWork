772d83396a5b4a4af833bed0f243049afc64243a
GrantDit <kiruha5854@mail.ru>
2023-05-03 16:13:59 +0300

    TestHomeWork1

---
+++ src/main/java/pro/sky/testshomework/TestsHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/testshomework/TestsHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.testshomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
